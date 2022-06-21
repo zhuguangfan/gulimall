fc496b4adf96a1453fa9ff9e89f317dab6e6c354
fan <135716418@qq.com>
2022-06-21 18:44:04 +0800

    提交聚合项目服务

---
+++ gulimall-coupon/src/test/java/com/atguigu/gulimall/oupon/GulimallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.gulimall.oupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
