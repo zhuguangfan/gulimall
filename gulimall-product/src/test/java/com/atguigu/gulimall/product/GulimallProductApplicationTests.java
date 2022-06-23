594c83044b4243d86436da685ea8440f3264daf0
fan <135716418@qq.com>
2022-06-23 11:21:36 +0800

    更新

--- gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
@@ -3,12 +3,11 @@ package com.atguigu.gulimall.product;
 import com.atguigu.gulimall.product.entity.BrandEntity;
 import com.atguigu.gulimall.product.service.BrandService;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+//@RunWith(SpringRunner.class)
 @SpringBootTest
 class GulimallProductApplicationTests {
 
@@ -18,7 +17,7 @@ class GulimallProductApplicationTests {
     @Test
     void contextLoads() {
         BrandEntity entity = new BrandEntity();
-        entity.setName("化为");
+        entity.setName("三星");
         brandService.save(entity);
         System.out.println("保存成功....");
     }
